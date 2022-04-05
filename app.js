1d32c48e523d398f30b9f46b3047fac8c848ee8f
Alain Box <alainbox@MBP-van-Alain.home>
2022-04-05 17:59:40 +0200

    change port listener

--- app.js
+++ app.js
@@ -135,11 +135,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+const PORT = process.env.PORT || 8000;
 
-app.listen(port, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(PORT, () => { console.log(`App listening on port ${PORT}!`); });
